4945829b6390d3b2698097ccea1e6969a55deb62
Loganathan G P <logusivam26@gmail.com>
2024-12-18 10:51:07 +0000

    Dec 18, 2024, 4:21 PM

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const { signup, verifyOTP } = require('../controllers/authController');
 const router = express.Router();
-const User = require('../models/User'); // Import User model
+//const User = require('../models/User');  Import User model
 
 // Route to handle user signup
 /* router.post('/signup', async (req, res) => {
