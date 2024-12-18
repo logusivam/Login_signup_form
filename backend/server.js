c39b95411324b2064c71c0ef040f047bcadc82ee
Loganathan G P <logusivam26@gmail.com>
2024-12-18 08:47:03 +0000

    Dec 18, 2024, 2:17 PM

--- backend/server.js
+++ backend/server.js
@@ -3,10 +3,18 @@ const dotenv = require('dotenv');
 const bodyParser = require('body-parser');
 const connectDB = require('./utils/db');
 const authRoutes = require('./routes/auth');
+//for country detail 
+const { getNames } = require('country-list');
 
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+// Route to get country list
+app.get('/api/countries', (req, res) => {
+    const countries = getNames(); // Get a list of country names
+    res.json(countries);
+});
 
 // Middleware
 app.use(bodyParser.json());
