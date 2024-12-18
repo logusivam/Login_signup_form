7799078d3e862d756fae58b908401bd4336cba33
Loganathan G P <logusivam26@gmail.com>
2024-12-18 09:47:18 +0000

    Dec 18, 2024, 3:17 PM

--- backend/server.js
+++ backend/server.js
@@ -9,6 +9,16 @@ const { countries } = require('countries-list');
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
+const path = require('path'); // Add this to handle file paths
+
+// Serve static files from the 'Register' directory
+app.use(express.static(path.join(__dirname, '../Register')));
+
+// Serve static index.html for undefined routes
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../Register/register.html'));
+});
+
 
 // Route to get countries and phone codes
 app.get('/api/countries', (req, res) => {
