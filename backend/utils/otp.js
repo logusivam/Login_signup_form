2401b95e8595d3d5474d4fa60cb1746bef33b18d
Loganathan G P <logusivam26@gmail.com>
2024-12-19 08:50:10 +0000

    Dec 19, 2024, 2:20 PM

--- backend/utils/otp.js
+++ backend/utils/otp.js
@@ -30,62 +30,6 @@ const sendOTPEmail = async (email, otp) => {
 
 module.exports = { generateOTP, sendOTPEmail };
 
-old code
- */
-
-const nodemailer = require('nodemailer');
-const { google } = require('googleapis');
-require('dotenv').config();
-
-// OAuth2 configuration
-const OAuth2 = google.auth.OAuth2;
-const oauth2Client = new OAuth2(
-    process.env.GMAIL_CLIENT_ID,
-    process.env.GMAIL_CLIENT_SECRET,
-    'https://developers.google.com/oauthplayground'
-);
-
-// Set credentials for OAuth2
-oauth2Client.setCredentials({
-    refresh_token: process.env.GMAIL_REFRESH_TOKEN,
-});
-
-// Create transporter using OAuth2
-const transporter = nodemailer.createTransport({
-    service: 'gmail',
-    auth: {
-        type: 'OAuth2',
-        user: process.env.EMAIL_USER, // Your email address
-        clientId: process.env.GMAIL_CLIENT_ID,
-        clientSecret: process.env.GMAIL_CLIENT_SECRET,
-        refreshToken: process.env.GMAIL_REFRESH_TOKEN,
-        accessToken: async () => {
-            const tokens = await oauth2Client.getAccessToken();
-            return tokens.token;
-        },
-    },
-});
-
-// Email utility to send OTP
-const sendOtpEmail = async (email, otp) => {
-    const mailOptions = {
-        from: process.env.EMAIL_USER,
-        to: email,
-        subject: 'Your OTP for Verification',
-        text: `Your OTP for email verification is: ${otp}. It will expire in 5 minutes.`,
-    };
-
-    try {
-        await transporter.sendMail(mailOptions);
-        console.log(`OTP sent successfully to ${email}`);
-    } catch (error) {
-        console.error('Error sending OTP:', error);
-        throw error;
-    }
-};
-
-module.exports = { sendOtpEmail };
-
 
 /*  directly sending maails code
 // Email utility to send OTP
