7bc859d3033fa71ea133751a0b00885f96e7734c
Loganathan G P <logusivam26@gmail.com>
2024-12-24 05:50:02 +0000

    Dec 24, 2024, 11:20 AM

--- Forget_Password/forgetpassword.js
+++ Forget_Password/forgetpassword.js
@@ -1,6 +1,9 @@
 const emailInput = document.getElementById('email');
 const verifyBtn = document.getElementById('sendOtpBtn');
-const otpInputs = document.querySelectorAll('.otp-digit');
+const otpInput = document.querySelectorAll('.otp-digit');
+const verifyOtpBtn = document.getElementById('verifyOtpBtn');
+const sendPasswordBtn = document.getElementById('sendPasswordBtn');
+const statusText = document.getElementById('status');
 
 // Enable Verify Button if the Email is Valid
 emailInput.addEventListener('input', () => {
@@ -15,16 +18,16 @@ emailInput.addEventListener('input', () => {
 });
 
 // Move focus to the next OTP input field
-otpInputs.forEach((input, index) => {
+otpInput.forEach((input, index) => {
     input.addEventListener('input', (e) => {
-        if (e.target.value && index < otpInputs.length - 1) {
-            otpInputs[index + 1].focus();
+        if (e.target.value && index < otpInput.length - 1) {
+            otpInput[index + 1].focus();
         }
     });
 
     input.addEventListener('keydown', (e) => {
         if (e.key === 'Backspace' && !e.target.value && index > 0) {
-            otpInputs[index - 1].focus();
+            otpInput[index - 1].focus();
         }
     });
 });
