db7b552125c6ad154d992b685b898a6567ba907a
Loganathan G P <logusivam26@gmail.com>
2024-12-30 08:01:13 +0000

    Dec 30, 2024, 1:31 PM

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -4,7 +4,7 @@ const { signup, loginUser, sendOtp, verifyOtp,  resendOtp
 const { sendForgetPasswordOtp, verifyForgetPasswordOtp, updatePassword, sendPassword } = require('../controllers/authController'); 
 const router = express.Router();
 const verifyToken = require('../utils/authMiddleware');
-const { logDebug } = require('..logger/logger');
+const { logDebug } = require('../logger');
 
 //sign-up page 
 router.post('/signup', signup);
